c3d41081d6a58a4aad61ebbfb4a07bd070cdd6c3
Bipin Thapa <yukichin638@gmail.com>
2024-12-07 21:41:15 +0545

    Day 50

--- Node Js/Practice/submit and receive.js
+++ Node Js/Practice/submit and receive.js
@@ -1,27 +1,65 @@
+let result;
+let dataObj = {};
+
+function add(){
+  let num1 = Number(document.querySelector('.num1').value);
+  let num2 = Number(document.querySelector('.num2').value);
+  result = num1 + num2;
+}
+
+function sub(){
+  let num1 = Number(document.querySelector('.num1').value);
+  let num2 = Number(document.querySelector('.num2').value);
+  result = num1 - num2;
+}
+
+function mul(){
+  let num1 = Number(document.querySelector('.num1').value);
+  let num2 = Number(document.querySelector('.num2').value);
+  result = num1 * num2;
+}
+
+function div(){
+  let num1 = Number(document.querySelector('.num1').value);
+  let num2 = Number(document.querySelector('.num2').value);
+  result = num1 / num2;
+}
+
 let a = (req,res) =>{
   if(req.url === '/'){
     res.setHeader('Content-Type','text/html');
     res.write(`
       <h1>Calculator</h1>
       <form method="POST" action="/calculate">
-        <input type="text" name="num1" placeholder="Enter 1st number">
-        <input type="text" name="num2" placeholder="Enter 2nd number">
+        <input type="text" name="num1" class = "num1" placeholder="Enter 1st number" style="height: 40px;">
+        <input type="text" name="num2" class = "num2" placeholder="Enter 2nd number" style="height: 40px;">
         <br>
-        <button>
-        <a href="/plus" style="text-decoration:none;">+<a></button>
-        <button>
-        <a href="/minus" style="text-decoration:none;">-<a></button>
-        <button>
-        <a href="/mul" style="text-decoration:none;">*<a></button>
-        <button>
-        <a href="/div" style="text-decoration:none;">/<a></button>
+        <br>
+        <div style="display: flex; column-gap: 10px;">
+          <button style="width:40px; height: 40px; font-size: 20px; text-align: center; cursor: pointer;" onclick="add()"><a href="/result" style="text-decoration:none;">+</a></button>
+          <button style="width:40px; height: 40px; font-size: 20px; text-align: center; cursor: pointer;" onclick="sub()"><a href="/result" style="text-decoration:none;">-</a></button>
+          <button style="width:40px; height: 40px; font-size: 20px; text-align: center; cursor: pointer;" onclick="mul()"><a href="/result" style="text-decoration:none;">*</a></button>
+          <button style="width:40px; height: 40px; font-size: 20px; text-align: center; cursor: pointer;" onclick="div()"><a href="/result" style="text-decoration:none;">/</a></button>
+        </div>
       </form>
     `);
     res.end();
   }
-  else if(req.url === '/plus' && req.method === 'POST'){
+  else if(req.url === '/result'){
     res.setHeader('Content-Type','text/html');
-    
+    const inputData = [];
+    req.on('data',(chunk)=>{
+      inputData.push(chunk);
+    });
+    req.on('end',()=>{
+      let data = Buffer.concat(inputData).toString();
+      let param = new URLSearchParams(data);
+      dataObj = Object.fromEntries(param);
+      dataObj.result = result;
+      res.write(`<h1>Result: ${result}</h1>
+      `);
+      res.end();
+    });
   }
   else{
     res.setHeader('Content-Type','text/html');
