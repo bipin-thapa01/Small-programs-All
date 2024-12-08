0a6c9eff8e0f4a4934333e9ddd7b948f39b1280a
Bipin Thapa <yukichin638@gmail.com>
2024-12-08 21:51:36 +0545

    Day 51

--- Node Js/Practice/submit and receive.js
+++ Node Js/Practice/submit and receive.js
@@ -1,33 +1,9 @@
+const fs = require('fs');
 let result;
 let dataObj = {};
-
-function add(){
-  let num1 = Number(document.querySelector('.num1').value);
-  let num2 = Number(document.querySelector('.num2').value);
-  result = num1 + num2;
-}
-
-function sub(){
-  let num1 = Number(document.querySelector('.num1').value);
-  let num2 = Number(document.querySelector('.num2').value);
-  result = num1 - num2;
-}
-
-function mul(){
-  let num1 = Number(document.querySelector('.num1').value);
-  let num2 = Number(document.querySelector('.num2').value);
-  result = num1 * num2;
-}
-
-function div(){
-  let num1 = Number(document.querySelector('.num1').value);
-  let num2 = Number(document.querySelector('.num2').value);
-  result = num1 / num2;
-}
-
-let a = (req,res) =>{
-  if(req.url === '/'){
-    res.setHeader('Content-Type','text/html');
+let a = (req, res) => {
+  if (req.url === '/') {
+    res.setHeader('Content-Type', 'text/html');
     res.write(`
       <h1>Calculator</h1>
       <form method="POST" action="/calculate">
@@ -36,33 +12,51 @@ let a = (req,res) =>{
         <br>
         <br>
         <div style="display: flex; column-gap: 10px;">
-          <button style="width:40px; height: 40px; font-size: 20px; text-align: center; cursor: pointer;" onclick="add()"><a href="/result" style="text-decoration:none;">+</a></button>
-          <button style="width:40px; height: 40px; font-size: 20px; text-align: center; cursor: pointer;" onclick="sub()"><a href="/result" style="text-decoration:none;">-</a></button>
-          <button style="width:40px; height: 40px; font-size: 20px; text-align: center; cursor: pointer;" onclick="mul()"><a href="/result" style="text-decoration:none;">*</a></button>
-          <button style="width:40px; height: 40px; font-size: 20px; text-align: center; cursor: pointer;" onclick="div()"><a href="/result" style="text-decoration:none;">/</a></button>
+          <button style="width:40px; height: 40px; font-size: 20px; text-align: center; cursor: pointer;" type="submit" name="operation" value="sum">+</button>
+          <button style="width:40px; height: 40px; font-size: 20px; text-align: center; cursor: pointer;" name="operation" value="diff">-</button>
+          <button style="width:40px; height: 40px; font-size: 20px; text-align: center; cursor: pointer;" name="operation" value="mul">*</button>
+          <button style="width:40px; height: 40px; font-size: 20px; text-align: center; cursor: pointer;" name="operation" value="div">/</button>
         </div>
       </form>
     `);
     res.end();
   }
-  else if(req.url === '/result'){
-    res.setHeader('Content-Type','text/html');
+  else if (req.url === '/calculate') {
+    res.setHeader('Content-Type', 'text/html');
     const inputData = [];
-    req.on('data',(chunk)=>{
+    req.on('data', (chunk) => {
       inputData.push(chunk);
     });
-    req.on('end',()=>{
+    req.on('end', () => {
       let data = Buffer.concat(inputData).toString();
       let param = new URLSearchParams(data);
       dataObj = Object.fromEntries(param);
+      const operation = dataObj.operation;
+      switch (operation) {
+        case 'sum':
+          result = Number(dataObj.num1) + Number(dataObj.num2);
+          break;
+        case 'diff':
+          result = Number(dataObj.num1) - Number(dataObj.num2);
+          break;
+        case 'mul':
+          result = Number(dataObj.num1) * Number(dataObj.num2);
+          break;
+        case 'div':
+          result = Number(dataObj.num2) !== 0 ? result = Number(dataObj.num1) / Number(dataObj.num2) : 'Undifined result';
+      }
       dataObj.result = result;
+      fs.writeFile('user.txt', JSON.stringify(dataObj), (error) => {
+        console.log(`
+        Error: ${error}`);
+      });
       res.write(`<h1>Result: ${result}</h1>
       `);
       res.end();
     });
   }
-  else{
-    res.setHeader('Content-Type','text/html');
+  else {
+    res.setHeader('Content-Type', 'text/html');
     res.write(`
     <h1>Error 404</h1>
     <p>Page not Found</p>`);
