d61882e515a815c2fa19b0617cf3e6ef4175985f
Bipin Thapa <yukichin638@gmail.com>
2024-12-28 11:58:31 +0545

    Day 2

---
+++ DSA/binary_search.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a[] = {1,2,3,4,5,6,7,8,9,10}, key, l = 0, n = 10, mid;
+	cout << "Enter the key: ";
+	cin >> key;
+	while(l<=n){
+		mid = (l+n)/2;
+		if(key == a[mid]){
+			cout << "Key found at " << mid+1;
+			exit(0);
+		}
+		else if(key<a[mid]){
+			n = mid-1;
+		}
+		else{
+			l = mid+1;
+		}
+	}
+	cout << "Key not found!";
+	return 0;
+}
